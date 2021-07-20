a901bbf3454661f34c65499791f8cb15401b6521
hiroki.nukaga <nkg145fb@gmail.com>
2021-07-21 00:13:11 +0900

    FrontController作成

--- src/main/java/controllers/FrontController.java
+++ src/main/java/controllers/FrontController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,7 +9,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name="FrontController", urlPatterns={"/"})
+import actions.ActionBase;
+import actions.UnknownAction;
+import constants.ForwardConst;
+
+
+
+/*
+ * フロントコントローラー
+ */
+@WebServlet("/")
 public class FrontController extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
@@ -18,10 +28,50 @@ public class FrontController extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        //パラメータに該当するActionクラスのインスタンス
+        ActionBase action = getAction(request,response);
+
+        //サーブレットコンテキスト,リクエスト,レスポンスをActionインスタンスのフィールドに限定
+        action.init(getServletContext(),request,response);
+
+        //Actionクラスの処理を呼び出し
+        action.process();
     }
+
+
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
     }
+
+
+    /*
+     * リクエストパラメータの値から該当するActionクラスのインスタンスを作成し、返却する。
+     * （例：パラメータがaction=Employeeの場合、action.EmployeeActionオブジェクト）
+     * @param request リクエスト
+     * @param response レスポンス
+     * @return
+     */
+    @SuppressWarnings({"rawtypes","unchecked"}) //コンパイラ警告を抑制
+    private ActionBase getAction(HttpServletRequest request,HttpServletResponse response) {
+        Class type =null;
+        ActionBase action = null;
+        try {
+            //リクエストからパラメータ"action"の値を取得（例："Employee","Report")
+            String actionString = request.getParameter(ForwardConst.ACT.getValue());
+
+            //該当するActionオブジェクトを作成（例：リクエストからパラメータaction＝Employeeの場合、action.EmployeeActionオブジェクト）
+            type = Class.forName(String.format("action.%sAction", actionString));
+
+            //ActionBaseのオブジェクトにキャスト(例：actions.EmployeeActionオブジェクト→Actions.ActionBaseオブジェクト)
+            action = (ActionBase) (type.asSubclass(ActionBase.class)
+                    .getDeclaredConstructor()
+                    .newInstance());
+        }catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SecurityException
+                | IllegalArgumentException | InvocationTargetException| NoSuchMethodException e) {
+            action = new UnknownAction();
+            }
+        return action;
+        }
 
 }
