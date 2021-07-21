780d9fba053c334a78d9ef5f5c123057071db735
hiroki.nukaga <nkg145fb@gmail.com>
2021-07-21 14:36:02 +0900

    従業員newアクションの作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -57,4 +57,17 @@ public class EmployeeAction extends ActionBase {
         forward(ForwardConst.FW_EMP_INDEX);
 
     }
+    /*
+     * 新規登録画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void entryNew() throws ServletException,IOException{
+
+        putRequestScope(AttributeConst.TOKEN,getTokenId());//CSRF対策
+        putRequestScope(AttributeConst.EMPLOYEE,new EmployeeView());
+
+        //新規登録画面を表示
+        forward(ForwardConst.FW_EMP_NEW);
+    }
 }
