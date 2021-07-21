2b8c27ecbecdd5edc71f476b40885bca7013b58e
hiroki.nukaga <nkg145fb@gmail.com>
2021-07-21 13:40:42 +0900

    従業員indexアクションの作成

--- src/main/java/controllers/FrontController.java
+++ src/main/java/controllers/FrontController.java
@@ -61,7 +61,7 @@ public class FrontController extends HttpServlet {
             String actionString = request.getParameter(ForwardConst.ACT.getValue());
 
             //該当するActionオブジェクトを作成（例：リクエストからパラメータaction＝Employeeの場合、action.EmployeeActionオブジェクト）
-            type = Class.forName(String.format("action.%sAction", actionString));
+            type = Class.forName(String.format("actions.%sAction", actionString));
 
             //ActionBaseのオブジェクトにキャスト(例：actions.EmployeeActionオブジェクト→Actions.ActionBaseオブジェクト)
             action = (ActionBase) (type.asSubclass(ActionBase.class)
